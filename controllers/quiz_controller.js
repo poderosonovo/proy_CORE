d734c9aa5aaadc30264864447a0578894efc89e9
poderosonovo <cholopode@gmail.com>
2016-05-03 18:28:00 +0200

    Añadir preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,6 +1,18 @@
 
 var models = require('../models');
 
+
+// GET /quizzes
+exports.index = function(req, res, next) {
+	models.Quiz.findAll()
+		.then(function(quizzes) {
+			res.render('quizzes/index', {quizzes: quizzes});
+		})
+		.catch(function(error) {
+			next(error);
+		});
+};
+
 // Autoload el quiz asociado a :quizId
 exports.load = function(req, res, next, quizId) {
 	models.Quiz.findById(quizId)
@@ -14,18 +26,6 @@ exports.load = function(req, res, next, quizId) {
         })
         .catch(function(error) { next(error); });
 };
-
-// GET /quizzes
-exports.index = function(req, res, next) {
-	models.Quiz.findAll()
-		.then(function(quizzes) {
-			res.render('quizzes/index.ejs', { quizzes: quizzes});
-		})
-		.catch(function(error) {
-			next(error);
-		});
-};
-
 
 // GET /quizzes/:id
 exports.show = function(req, res, next) {
@@ -47,4 +47,25 @@ exports.check = function(req, res, next) {
 	res.render('quizzes/result', { quiz: req.quiz, 
 								   result: result, 
 								   answer: answer });
+};
+
+// GET /quizzes/new
+exports.new = function(req, res, next) {
+  var quiz = models.Quiz.build({question: "", answer: ""});
+  res.render('quizzes/new', {quiz: quiz});
+};
+
+// POST /quizzes/create
+exports.create = function(req, res, next) {
+  var quiz = models.Quiz.build({ question: req.body.quiz.question, 
+  	                             answer:   req.body.quiz.answer} );
+
+// guarda en DB los campos pregunta y respuesta de quiz
+  quiz.save({fields: ["question", "answer"]})
+  	.then(function(quiz) {
+    	res.redirect('/quizzes');  // res.redirect: Redirección HTTP a lista de preguntas
+    })
+    .catch(function(error) {
+		next(error);
+	});  
 };
