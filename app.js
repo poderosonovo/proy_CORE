681b81d985adad3d0cc80d89a86e7df7744c80a1
poderosonovo <cholopode@gmail.com>
2016-05-03 19:38:39 +0200

    Editar preguntas

--- app.js
+++ app.js
@@ -4,31 +4,33 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var partials = require('express-partials');
+var flash = require('express-flash');
+var methodOverride = require('method-override');
 
 var routes = require('./routes/index');
 
 var app = express();
-
-var session= require ('express-session');
-var partials= require ('express-partials');
-var flash= require('express-flash');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-app.use(partials());
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(session({secret: "Quiz 2016",
-                  resave: false,
-                  saveUnitialized: true}));
-app.use(flash());
+                 resave: false,
+                 saveUninitialized: true}));
+app.use(methodOverride('_method', {methods: ["POST", "GET"]}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(partials());
+app.use(flash());
 
 app.use('/', routes);
 
